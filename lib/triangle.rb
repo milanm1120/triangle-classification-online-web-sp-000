d6095eeb3e8865524e9f591e9c853cd3be961ea1
Milan Mody <milanm1120@gmail.com>
2021-03-25 03:13:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
 
    def kind
      valid?
-     if side_a == side_b && side_b == side_c
+     if side_a == side_b && side_b == side_c && side_a == side_c
        :equilateral
      elsif side_a == side_b || side_b == side_c || side_a == side_c
        :isosceles
