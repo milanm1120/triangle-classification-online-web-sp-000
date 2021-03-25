98067c86ca084d0777635503567f64ffd314bcc3
Milan Mody <milanm1120@gmail.com>
2021-03-25 03:23:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,13 +21,13 @@ class Triangle
       end
     end
 
-    def valid?
-      real_triangle = [(side_a + side_b > side_c), (side_a + side_c > side_b), (side_b + side_c > side_a)]
-      [side_a, side_b, side_c].each do |side|
-        real_triangle << false if side <= 0
-        raise TriangleError if real_triangle.include?(false)
-      end
+  def valid?
+    real_triangle = [(side_a + side_b > side_c), (side_a + side_c > side_b), (side_b + side_c > side_a)]
+    [side_a, side_b, side_c].each do |side|
+      real_triangle << false if side <= 0
+      raise TriangleError if real_triangle.include?(false)
     end
+  end
 
 class TriangleError < StandardError
 
